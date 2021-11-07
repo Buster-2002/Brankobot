8dfe82af901b1871dbd6afdf6fc90ac8c5249cc1
Pepijn Stemerding <mysteriousduck2002@gmail.com>
2021-11-07 15:10:40 +0100

    improved docstrings

--- brankobot/cogs/utils/converters.py
+++ brankobot/cogs/utils/converters.py
@@ -39,11 +39,28 @@ from .errors import (InvalidCommandName, NotARegion, NoTimeFound,
 class RegionConverter(commands.Converter):
     '''Converts a region argument into Region
 
+    Example
+    -------
     >>> await RegionConverter().convert(ctx, "eu")
     <Region.eu: 'eu'>
 
     >>> await RegionConverter().convert(ctx, "RuSsIA")
     <Region.ru: 'ru'>
+
+    Parameters
+    ----------
+    argument : str
+        The argument as passed to the converter
+
+    Returns
+    -------
+    Region
+        The region converted from string
+
+    Raises
+    ------
+    NotARegion
+        The region argument wasn't found
     '''
     async def convert(self, _, argument: str) -> Region:
         argument = argument.lower()
@@ -69,11 +86,13 @@ class RegionConverter(commands.Converter):
 
 
 class CommandNameCheck(commands.Converter):
-    '''Checks if the command name is valid
+    '''Checks if the command name is of a valid format
 
     This potentially saves having to look through database
     if the format is wrong
 
+    Example
+    -------
     >>> await CommandNameCheck().convert(ctx, "#!$!$")
     Traceback (most recent call last):
         File "<stdin>", line 1, in <module>
@@ -81,27 +100,71 @@ class CommandNameCheck(commands.Converter):
 
     >>> await CommandNameCheck().convert(ctx, "bubby")
     bubby
+
+    Parameters
+    ----------
+    argument : str
+        The argument as passed to the converter
+
+    Returns
+    -------
+    str
+        The input argument if valid
+
+    Raises
+    ------
+    InvalidCommandName
+        The argument didn't consist of juist spaces and latin
+        characters, and/or wasn't at most 50 chars long
     '''
     async def convert(self, _, argument: str) -> str:
         if not re.match(r'^[\w ]{1,50}$', argument):
             raise InvalidCommandName()
+
         return argument
 
 
 class ReminderConverter(commands.Converter):
-    '''Extracts a time from argument
+    '''Extracts a time from argument and converts it
+    into a datetime
 
-    >>> await ReminderConverter().convert(ctx, "In 3 hours, do the dishes")
-    ('do the dishes', datetime.datetime(2021, 10, 3, 15, 22, 19, 139779))
+    Example
+    -------
+    >>> datetime.now()
+    datetime.datetime(2021, 11, 7, 14, 13, 38, 699029)
+    >>> await ReminderConverter().convert(ctx, 'in 3 hours do the dishes')
+    ('do the dishes', datetime.datetime(2021, 11, 7, 17, 13, 43, 61210))
+
+    Parameters
+    ----------
+    argument : str
+        The argument as passed to the converter
+
+    Returns
+    -------
+    Tuple[str, datetime]
+        The part of the argument that wasn't part of the extracted time
+
+    Raises
+    ------
+    NoTimeFound
+        Couldn't find a time in the argument
+    TimeTravelNotPossible
+        The time argument wasn't prefixed with 'in' so the parser
+        interprets it as a time in the past
     '''
+    @staticmethod
+    def _clean_argument(argument: str, detected: str) -> str:
+        return argument.replace(detected, '').strip().lstrip(', ')
+
     async def convert(self, _, argument: str) -> Tuple[str, datetime]:
-        dates = search_dates(argument, ['en'])
+        dates = search_dates(argument, languages=['en'])
         now = datetime.now()
         if not dates:
             raise NoTimeFound('Couldn\'t find a time in your argument')
 
         detected, date = dates[0]
         if now > date:
-            raise TimeTravelNotPossible(date)
+            raise TimeTravelNotPossible(detected, date)
 
-        return argument.replace(detected, '').strip(), date
+        return self._clean_argument(argument, detected), date
