e33e6e122b4eb05ba066549886a9a0126ad576a1
Pepijn Stemerding <mysteriousduck2002@gmail.com>
2021-11-12 10:43:54 +0100

    exclude owners from checks

--- brankobot/cogs/utils/checks.py
+++ brankobot/cogs/utils/checks.py
@@ -49,29 +49,29 @@ def is_moderator(ctx: Context) -> bool:
         True if user has xo, po or co role
         False if not
     '''
-    return True
-    # moderator_roles = {
-    #     BigRLDRoleType.xo,
-    #     SmallRLDRoleType.xo,
-    #     BigRLDRoleType.po,
-    #     SmallRLDRoleType.po,
-    #     BigRLDRoleType.co,
-    #     SmallRLDRoleType.co
-    # }
-    # allowed_ids = {mr.value for mr in moderator_roles}
-    # return any([r.id in allowed_ids for r in ctx.author.roles])
+    moderator_roles = {
+        BigRLDRoleType.xo,
+        SmallRLDRoleType.xo,
+        BigRLDRoleType.po,
+        SmallRLDRoleType.po,
+        BigRLDRoleType.co,
+        SmallRLDRoleType.co
+    }
+    allowed_ids = {mr.value for mr in moderator_roles}
+    return any([r.id in allowed_ids for r in ctx.author.roles])
 
 
 def is_connected():
     '''Returns whether the invoker is connected to a voice channel
     '''    
     async def predicate(ctx: Context) -> bool:
-        return True
-        # destination = getattr(ctx.author.voice, 'channel', None)
-        # if destination is None:
-        #     raise VoiceChannelError(f'You aren\'t connected to a voice channel')
+        if (await ctx.bot.is_owner(ctx.author)) is False:
+            destination = getattr(ctx.author.voice, 'channel', None)
 
-        # return True
+            if destination is None:
+                raise VoiceChannelError(f'You aren\'t connected to a voice channel')
+
+        return True
 
     return commands.check(predicate)
 
@@ -96,14 +96,14 @@ def channel_check(*additional_channels: Tuple[Union[BigRLDChannelType, SmallRLDC
     ) + additional_channels
 
     async def predicate(ctx: Context) -> bool:
+        if (await ctx.bot.is_owner(ctx.author)) is False:
+            allowed_ids = {ct.value for ct in channel_types} | {869681728170639391, 859739694635679794}
+            ctx.command.allowed_channel_types = channel_types
+
+            if not ctx.channel.id in allowed_ids:
+                raise ChannelNotAllowed(allowed_ids)
+
         return True
-        # allowed_ids = {ct.value for ct in channel_types} | {869681728170639391, 859739694635679794}
-        # ctx.command.allowed_channel_types = channel_types
-
-        # if ctx.channel.id in allowed_ids:
-        #     return True
-
-        # raise ChannelNotAllowed(allowed_ids)
 
     return commands.check(predicate)
 
@@ -129,13 +129,13 @@ def role_check(*roles: Tuple[Union[BigRLDRoleType, SmallRLDRoleType]]) -> comman
     )
 
     async def predicate(ctx: Context) -> bool:
+        if (await ctx.bot.is_owner(ctx.author)) is False:
+            allowed_ids = {ct.value for ct in role_types}
+            ctx.command.allowed_role_types = role_types
+
+            if not any([r.id in allowed_ids for r in ctx.author.roles]):
+                raise MissingRoles(allowed_ids)
+
         return True
-        # allowed_ids = {ct.value for ct in role_types}
-        # ctx.command.allowed_role_types = role_types
-
-        # if any([r.id in allowed_ids for r in ctx.author.roles]):
-        #     return True
-
-        # raise MissingRoles(allowed_ids)
 
     return commands.check(predicate)
